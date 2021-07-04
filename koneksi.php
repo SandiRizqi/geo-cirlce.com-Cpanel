2b9462705d2f93194ee58740b53a048c95937ec8
Anugrah Sandi Rizqi <anugrah.sandi@mail.ugm.ac.id>
2021-07-04 17:33:53 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$database = "u4961916_users";
-$username = "u4961916_sandi";
-$password = "k@U@aUq4a]wI";
+$database = "*******";
+$username = "*******";
+$password = "******";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
